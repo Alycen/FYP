aa0dfe700ca37d36f06a21442fab9a375cf18888
Alycen <alsunnid@gmail.com>
2015-10-30 23:33:55 +0000

    saving minor changes

--- AttemptI/GameScene.h
+++ AttemptI/GameScene.h
@@ -3,11 +3,14 @@
 #define GAMESCENE_H
 
 #include "BaseScene.h"
+#include "Player.h"
+#include "NPC.h"
 
 class GameScene : public BaseScene
 {
 private:
-
+	Player player;
+	NPC npc_01;
 public:
 
 };
